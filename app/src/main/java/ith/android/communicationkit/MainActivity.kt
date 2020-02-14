f7fcb38c8d553728c8fb0112b9c6eba0d0fbdd31
Narek Dallakyan <narekdallakyanwork@gmail.com>
2020-02-14 05:02:08 +0400

    Setup ITH Communication Kit Android.

---
+++ app/src/main/java/ith/android/communicationkit/MainActivity.kt
@@ -0,0 +1,12 @@
+package ith.android.communicationkit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
