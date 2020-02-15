68dbbca12daaa81c85b7ea7646182d15f8568e77
Narek Dallakyan <narekdallakyanwork@gmail.com>
2020-02-15 15:22:52 +0400

    Created signaling mechanism.

--- app/src/main/java/dappy/signal/kit/MainActivity.kt
+++ app/src/main/java/dappy/signal/kit/MainActivity.kt
@@ -2,48 +2,10 @@ package dappy.signal.kit
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import dappy.signal.kit.constants.ServerInformation
-import dappy.signal.kit.model.Signal
-import dappy.signal.kit.observable.SignalAction
-import dappy.signal.kit.observable.SignalActionObserver
-import dappy.signal.kit.socket.SignalOption
-import dappy.signal.kit.socket.SocketIO
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startSignaling()
-    }
-
-    private fun startSignaling() {
-        val signalAction: SignalAction = SocketIO()
-            .observe(object : SignalActionObserver {
-                override fun onConnected() {
-                    println()
-                }
-
-                override fun onDisconnected() {
-                    println()
-                }
-
-                override fun onError(errorMessage: String) {
-                    println()
-                }
-
-                override fun onSignal(signal: Signal) {
-                    println()
-                }
-
-            })
-            .connect(
-                SignalOption(
-                    ServerInformation.SERVER_URL,
-                    "signal",
-                    true
-                )
-            )
-
     }
 }
--- app/src/main/java/dappy/signal/kit/constants/Constants.kt
+++
@@ -1,5 +0,0 @@
-package dappy.signal.kit.constants
-
-object ServerInformation {
-    const val SERVER_URL = "http://5.9.1.58:3001"
-}
